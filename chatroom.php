3a7dda08961984d6b26b4584d79bcd1d9f6c6e46
Oreigdor Catropolis <sethfb@gmail.com>
2018-09-21 08:18:59 -0400

    Update chatroom.php

--- chatroom.php
+++ chatroom.php
@@ -4,14 +4,43 @@
 <title>Chat - Customer Module</title>
 <link type="text/css" rel="stylesheet" href="chatroom.css" />
 </head>
+<?
+session_start();
  
+function loginForm(){
+    echo'
+    <div id="loginform">
+    <form action="index.php" method="post">
+        <p>Please enter your name to continue:</p>
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name" />
+        <input type="submit" name="enter" id="enter" value="Enter" />
+    </form>
+    </div>
+    ';
+}
+ 
+if(isset($_POST['enter'])){
+    if($_POST['name'] != ""){
+        $_SESSION['name'] = stripslashes(htmlspecialchars($_POST['name']));
+    }
+    else{
+        echo '<span class="error">Please type in a name</span>';
+    }
+}
+?>
+ <?php
+if(!isset($_SESSION['name'])){
+    loginForm();
+}
+else{
+?>
 <div id="wrapper">
     <div id="menu">
-        <p class="welcome">Welcome, <b></b></p>
+        <p class="welcome">Welcome, <b><?php echo $_SESSION['name']; ?></b></p>
         <p class="logout"><a id="exit" href="#">Exit Chat</a></p>
         <div style="clear:both"></div>
-    </div>
-     
+    </div>    
     <div id="chatbox"></div>
      
     <form name="message" action="">
@@ -23,8 +52,35 @@
 <script type="text/javascript">
 // jQuery Document
 $(document).ready(function(){
- 
+	//If user wants to end session
+	$("#exit").click(function(){
+		var exit = confirm("Are you sure you want to end the session?");
+		if(exit==true){window.location = 'index.php?logout=true';}		
+	});
+ //If user submits the form
+	$("#submitmsg").click(function(){	
+		var clientmsg = $("#usermsg").val();
+		$.post("post.php", {text: clientmsg});				
+		$("#usermsg").attr("value", "");
+		return false;
+	});
 });
+</script>
+ <?php
+     if(isset($_GET['logout'])){ 
+     
+    //Simple exit message
+    $fp = fopen("log.html", 'a');
+    fwrite($fp, "<div class='msgln'><i>User ". $_SESSION['name'] ." has left the chat session.</i><br></div>");
+    fclose($fp);
+     
+    session_destroy();
+    header("Location: index.php"); //Redirect the user
+}
+     ?>
+<?php
+}
+?>
 </script>
 </body>
 </html>
