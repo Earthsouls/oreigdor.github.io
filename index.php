5514bff5b1e5be6293353bdaa8b2ab934c789038
Oreigdor Catropolis <sethfb@gmail.com>
2020-05-25 13:03:55 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -1,111 +1,48 @@
-<?php
-session_start();
-
-if(isset($_GET['logout'])){	
-	
-	//Simple exit message
-	$fp = fopen("log.html", 'a');
-	fwrite($fp, "<div class='msgln'><i>User ". $_SESSION['name'] ." has left the chat session.</i><br></div>");
-	fclose($fp);
-	
-	session_destroy();
-	header("Location: chatroom.php"); //Redirect the user
-}
-
-function loginForm(){
-	echo'
-	<div id="loginform">
-	<form action="chatroom.php" method="post">
-		<p>Please enter your name to continue:</p>
-		<label for="name">Name:</label>
-		<input type="text" name="name" id="name" />
-		<input type="submit" name="enter" id="enter" value="Enter" />
-	</form>
-	</div>
-	';
-}
-
-if(isset($_POST['enter'])){
-	if($_POST['name'] != ""){
-		$_SESSION['name'] = stripslashes(htmlspecialchars($_POST['name']));
-	}
-	else{
-		echo '<span class="error">Please type in a name</span>';
-	}
-}
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
-<title>Chat - Customer Module</title>
-<link type="text/css" rel="stylesheet" href="chatroom.css" />
-</head>
+	<title>Register for CHC</title>
+	<link rel="stylesheet" href="style.css" type="text/css" media="all" id="magazinetheme">
+	<script type="text/javascript" src="http://christianheightscamp.com/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp"></script>
+	<script type="text/javascript" src="http://christianheightscamp.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1"></script>
+	<link rel="icon" href="http://christianheightscamp.com/wp-content/uploads/2018/12/cropped-logo_new-1-32x32.png" sizes="32x32">
+	<link rel="icon" href="http://christianheightscamp.com/wp-content/uploads/2018/12/cropped-logo_new-1-192x192.png" sizes="192x192">
+	<link rel="apple-touch-icon" href="http://christianheightscamp.com/wp-content/uploads/2018/12/cropped-logo_new-1-180x180.png">
 
-<?php
-if(!isset($_SESSION['name'])){
-	loginForm();
-}
-else{
-?>
-<div id="wrapper">
-	<div id="menu">
-		<p class="welcome">Welcome, <b><?php echo $_SESSION['name']; ?></b></p>
-		<p class="logout"><a id="exit" href="#">Exit Chat</a></p>
-		<div style="clear:both"></div>
-	</div>	
-	<div id="chatbox"><?php
-	if(file_exists("log.html") && filesize("log.html") > 0){
-		$handle = fopen("log.html", "r");
-		$contents = fread($handle, filesize("log.html"));
-		fclose($handle);
-		
-		echo $contents;
-	}
-	?></div>
-	
-	<form name="message" action="">
-		<input name="usermsg" type="text" id="usermsg" size="63" />
-		<input name="submitmsg" type="submit"  id="submitmsg" value="Send" />
-	</form>
-</div>
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3/jquery.min.js"></script>
+</head>
+<body>
+	<div id="mainform">
+	<form action="submit.php" method="post">
+		<label for="name">Camper Name:</label><input type="text" name="name" id="name" placeholder="John Smith"><br>
+		<label for="dob">Birthdate:</label><input type="date" id="dob"><br>
+		<label for="age">Age:</label><input type="number" name="age"><br>
+		Biological gender:
+		<label for="male">Male</label><input id="male" type="radio" name="gender"><br>
+		<label for="male">Female</label><input id="female" type="radio" name="gender"><br><br>
+		<label for="address">Address:</label><input type="text" name="address" placeholder="123 Example St, Little Valley, NY 14755"><br>
+		<label for="phone">Phone #:</label><input type="tel" name="phone"><br>
+		<label for="size">Shirt size:</label><br>
+		<label for="ys">Youth Small</label><input type="radio" value="ys"id="ys" name="size"><br>
+		<label for="ym">Youth Medium</label><input type="radio" value="ym"id="ym" name="size"><br>
+		<label for="yl">Youth Large</label><input type="radio" value="yl"id="yl" name="size"><br>
+		<label for="as">Adult Medium</label><input type="radio" value="as"id="as" name="size"><br>
+		<label for="am">Adult Small</label><input type="radio" value="am"id="am" name="size"><br>
+		<label for="al">Adult Large</label><input type="radio" value="al"id="al" name="size"><br>
+		<label for="other">Other</label><input type="radio" id="other" name="size" value=""><input type="text" name="size"><br><br>
+		<label for="email">Email:</label><input type="email" name="email">
+		<p>Cost:<br>* $10 payable by Cash, Check or Venmo<br>Resources needed:<br>* Electronic device<br>* Reliable internet signal<br>* Positive Attitude!<br>* Imagination<br>* Various household items may be used in crafts/activities<br>Participant Acknowledgements:<br>I give permission for my child to access the Zoom platform using the password provided by Christian Heights Camp to participate in large and small group activities. I will not share the Christian Heights Camp log-in information to Zoom. If my child is under the age of 12, I agree to have an adult nearby who is available during the hours of virtual camp. I will set up my child&#39;s access to the internet/virtual camp in a place other than his/her bedroom. I will be aware of his/her background and what/who is visible. I release Christian Heights Camp from any responsibility for use of internet searches outside of camp channels. I agree to talk to my child about being online, such as safety, online searches, oversharing and cyber bulling (For some guidance, see pages 4-11 <a href="www.whitehouse.gov/wp-content/uploads/2018/05/">www.whitehouse.gov/wp-content/uploads/2018/05/</a>...<br>General Release:<br>I give permission for my child to virtually attend Christian Heights Camp and participate in the scheduled program. I recognize the possible risk involved for my child in participating in online activities, including risks associated with online piracy, security, and invasion of privacy. I release, discharge, and otherwise agree to indemnify Christian Heights Camp, its affiliated organizations and sponsors, its officers, directors, employees, volunteers, and agents against any claim by or on behalf of myself or my minor child as a result of my child&#39;s participation in any program or activity sponsored, coordinated, or supervised by Christian Heights Camp.<br> I give permission on behalf of myself and my minor child for photographs to be taken of my child to be used for publicity purposes by Christian Heights Camp. I [[consent]] agree on behalf of myself and my minor child that photographs are the property of Christian Heights Camp and Christian Heights Camp shall have the right to reproduce, publish in print, online, and/or via social media these photos and videos as they desire free and clear of any claims on my, or my minor child&#39;s, part. I further give permission and [[consent]] agree on behalf of myself and my minor child that any photographs may be published and used by Christian Heights Camp, to illustrate and promote the camp experience, Christian Heights, and its camp programs.<br> I give permission for the Christian Heights Camp staff and board of directors to discuss my child&#39;s health and any information I provide in the camper registration forms.<br> Recording on the Virtual Platform</p>
+		<label for="recordyes">Yes, I give permission for Christian Heights Camp to record virtual sessions where my child is present.</label><input id="recordyes" type="radio" name="record" value="yes">
+		<label for="recordno">No, I do not give permission for Christian Heights Camp to include my child when recording virtual sessions (The camp director will disable your child&#39;s camera and mute his/her audio during all recorded sessions.)</label><input type="radio" name="record" id="recordno" value="no">
+	<p>Camper Code of Conduct:<br>Our hope is that Christian Heights Camp will be a place for campers to make friends and find support.<br>In order for Christian Heights Camp to be safe, fun and enjoyable for everyone, there are some expectations to discuss with your child.<br> 1. I will choose the golden rule while at Christian Heights Camp.<br> 1. Alcoholic beverages, illegal drugs, smoking or tobacco products are not allowed during the hours of virtual camp. Christian Heights Camp has the right to remove a camper from the Zoom meeting without warning if the Camp Director suspects these products have been used.<br>3. Sexual or suggestive behavior/comments/conversations is not appropriate or acceptable at Christian Heights Camp.<br>4. Campers will only use the Zoom platform to contact camp staff and fellow campers during the scheduled camp times..<br> 5. Screen shots, recording, and private, separate chats are not allowed during virtual camp sessions.<br> 6. My background will be appropriate, such as, but not limited to, a blank wall, virtual background image, or pictures appropriate for camp.<br>7. I will follow the guidelines for appropriate chat conversations.<br>a. If you ever feel uncomfortable, tell your trusted adult<br>b. No secrets. (Surprises are different. Surprises make us feel good and are intended to share eventually. Secrets are not ever meant to share and can feel bad.)<br>c. Good messages are ones that parents/guardians are excited to see and hear and the person sharing it with you would want your parents/guardians to see and hear the message.<br> d. Cussing, swearing and foul language is not acceptable at Christian Heights Camp. <br> Electronic signature of this page shows you understand, agree, and consent with the above stated cost, resources needed, acknowledgements, general release and camper code of conduct.</p>
+<label for="parentname">Parent/Guardian Name:</label><input type="text" name="parentname">
+<script src="signature_pad.min.js"></script>
+<canvas id="signature-pad" class="signature-pad" width="300px" height="200px"></canvas>
 <script type="text/javascript">
-// jQuery Document
-$(document).ready(function(){
-	//If user submits the form
-	$("#submitmsg").click(function(){	
-		var clientmsg = $("#usermsg").val();
-		$.post("post.php", {text: clientmsg});				
-		$("#usermsg").attr("value", "");
-		return false;
-	});
-	
-	//Load the file containing the chat log
-	function loadLog(){		
-		var oldscrollHeight = $("#chatbox").attr("scrollHeight") - 20;
-		$.ajax({
-			url: "log.html",
-			cache: false,
-			success: function(html){		
-				$("#chatbox").html(html); //Insert chat log into the #chatbox div				
-				var newscrollHeight = $("#chatbox").attr("scrollHeight") - 20;
-				if(newscrollHeight > oldscrollHeight){
-					$("#chatbox").animate({ scrollTop: newscrollHeight }, 'normal'); //Autoscroll to bottom of div
-				}				
-		  	},
-		});
-	}
-	setInterval (loadLog, 2500);	//Reload file every 2.5 seconds
-	
-	//If user wants to end session
-	$("#exit").click(function(){
-		var exit = confirm("Are you sure you want to end the session?");
-		if(exit==true){window.location = 'index.php?logout=true';}		
-	});
-});
+	var signaturePad = new SignaturePad(document.getElementById('signature-pad'));
 </script>
-<?php
-}
-?>
+<input type="text" id="output" value="">
+<button onclick="document.getElementById('output').value=signaturePad.toDataURL('image/png');">Finish signature</button>
+<label for="submit">Submit</label><input type="submit">
+</form>
 </body>
 </html>
+
---
+++ submit.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>Register for CHC</title>
+	<link rel="stylesheet" href="style.css" type="text/css" media="all" id="magazinetheme">
+	<script type="text/javascript" src="http://christianheightscamp.com/wp-includes/js/jquery/jquery.js?ver=1.12.4-wp"></script>
+	<script type="text/javascript" src="http://christianheightscamp.com/wp-includes/js/jquery/jquery-migrate.min.js?ver=1.4.1"></script>
+</head>
+<body>
+<!--[if lt IE 9]>
+	<script type="text/javascript" src="libs/flashcanvas.js"></script>
+	<![endif]-->
+	<script src="libs/jSignature.min.js"></script>
+	<div id="signature"></div>
+	<script>
+	    $(document).ready(function() {
+	        $("#signature").jSignature()
+	    })
+	</script>
+</body>
+</html>
